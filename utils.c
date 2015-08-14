13b39a524dcb774200a511ad1d4f1a75cd1971e4
nmlgc <nmlgc@nmlgc.net>
2015-08-14 14:31:19 +0200

    Add a hash table that maps variable-size keys to constant-size values
    Yay for Greenspun's Tenth Rule.

--- utils.c
+++ utils.c
@@ -6,11 +6,145 @@
  * Author: Nmlgc
  */
 
+#include <assert.h>
+#include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "ao.h"
 #include "utils.h"
+
+/// Hash table
+/// ----------
+// From http://www.cse.yorku.ca/~oz/hash.html
+static unsigned long hash(const char *key_buf, size_t key_len)
+{
+	unsigned long hash = 5381;
+	int i;
+	for(i = 0; i < key_len; i++) {
+		hash = ((hash << 5) + hash) ^ key_buf[i]; // hash * 33 ^ c
+	}
+	return hash;
+}
+
+// MSVC has memicmp(), but GCC does not, so this has to have a different name.
+static int memcasecmp(const void *s1, const void *s2, size_t n)
+{
+	const char *sc1 = (const char *)s1;
+	const char *sc2 = (const char *)s2;
+	const char *sc2end = sc2 + n;
+
+	while (sc2 < sc2end && toupper(*sc1) == toupper(*sc2)) {
+		sc1++, sc2++;
+	}
+	if (sc2 == sc2end) {
+		return 0;
+	}
+	return (int) (toupper(*sc1) - toupper(*sc2));
+}
+
+static int bucket_match(const hashtable_bucket_t *bucket, const void *key_buf, size_t key_len, hashtable_flags_t flags)
+{
+	if(bucket->key_len != key_len) {
+		return 0;
+	}
+	return flags & HT_CASE_INSENSITIVE
+		? !memcasecmp(bucket->key_buf, key_buf, key_len)
+		: !memcmp(bucket->key_buf, key_buf, key_len);
+}
+
+void* hashtable_get(hashtable_t *table, const void *key_buf, size_t key_len, hashtable_flags_t flags)
+{
+	hashtable_bucket_t *bucket;
+	ao_bool create = flags & HT_CREATE;
+
+	assert(table);
+	assert(table->data_size > 0);
+	assert(key_buf);
+	assert(key_len);
+
+	bucket = &table->buckets[hash(key_buf, key_len) % BUCKETS];
+
+	while(bucket->data && !bucket_match(bucket, key_buf, key_len, flags)) {
+		if(!bucket->next && create) {
+			bucket->next = calloc(1, sizeof(hashtable_bucket_t));
+		}
+		bucket = bucket->next;
+	}
+	if(!bucket->data && create) {
+		bucket->data = calloc(1, table->data_size);
+		bucket->key_len = key_len;
+		bucket->key_buf = malloc(key_len);
+		memcpy(bucket->key_buf, key_buf, key_len);
+	}
+	return bucket->data;
+}
+
+void* hashtable_iterate(hashtable_t *table, hashtable_iterator_t *iter)
+{
+	hashtable_t *ret = NULL;
+	assert(table);
+	assert(iter);
+	while(iter->i < BUCKETS) {
+		if(!iter->bucket) {
+			iter->bucket = &table->buckets[iter->i];
+		}
+		if(iter->bucket->data) {
+			ret = iter->bucket->data;
+		}
+		if(iter->bucket->next) {
+			iter->bucket = iter->bucket->next;
+		} else {
+			iter->i++;
+			iter->bucket = NULL;
+		}
+		if(ret) {
+			return ret;
+		}
+	}
+	return ret;
+}
+
+unsigned int hashtable_length(hashtable_t *table)
+{
+	unsigned int ret = 0;
+	hashtable_iterator_t iter = {0};
+	assert(table);
+	while(hashtable_iterate(table, &iter)) {
+		ret++;
+	}
+	return ret;
+}
+
+void hashtable_free(hashtable_t *table)
+{
+	int i;
+	assert(table);
+	if(!table->buckets) {
+		return;
+	}
+	for(i = 0; i < BUCKETS; i++) {
+		// We only have to free the key of the first entry in each
+		// bucket's list, as it was allocated as part of a single array
+		// in hashtable_init().
+		hashtable_bucket_t *bucket = &table->buckets[i];
+		if(bucket->key_buf) {
+			free(bucket->key_buf);
+		}
+		bucket = bucket->next;
+		while(bucket) {
+			hashtable_bucket_t *bucket_cur = bucket;
+			bucket = bucket->next;
+			if(bucket_cur->data) {
+				free(bucket_cur->data);
+				free(bucket_cur->key_buf);
+			}
+			free(bucket_cur);
+		}
+	}
+}
+/// ----------
 
 FILE* fopen_derivative(const char *fn, const char *suffix)
 {
--- utils.h
+++ utils.h
@@ -9,6 +9,54 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+/// Hash table
+/// ----------
+// This hash table maps variable-size keys to constant-size values.  Before
+// calling hashtable_get(), the [data_size] member of hashtable_t has to be
+// initialized to the size of the data block that is to be mapped.
+#define BUCKETS 15
+
+typedef struct hashtable_bucket {
+	struct hashtable_bucket *next;
+	void *key_buf;
+	size_t key_len;
+	void *data;
+} hashtable_bucket_t;
+
+typedef struct {
+	unsigned int i;
+	hashtable_bucket_t *bucket;
+} hashtable_iterator_t;
+
+typedef struct {
+	size_t data_size;
+	hashtable_bucket_t buckets[BUCKETS];
+} hashtable_t;
+
+// Flags used for hash table lookup
+typedef enum {
+	HT_CREATE = 0x1, // create nonexisting entries
+	HT_CASE_INSENSITIVE = 0x2, // case-insensitive key lookup
+} hashtable_flags_t;
+
+// Looks up the entry in [table] with the given key and the given [flags].
+// Returns a writable pointer to the entry data, or NULL if the entry doesn't
+// exist and the HT_CREATE flag was not given.
+void* hashtable_get(hashtable_t *table, const void *key_buf, size_t key_len, hashtable_flags_t flags);
+
+// Iterates over all entries in [table], using [iter] to store the iteration
+// state.  [iter] should be cleared to 0 before the first call. Returns the
+// current data entry, or NULL if the end of [table] has been reached.
+void* hashtable_iterate(hashtable_t *table, hashtable_iterator_t *iter);
+
+// Returns the number of entries in [table].
+unsigned int hashtable_length(hashtable_t *table);
+
+// Frees the hash table, assuming that any allocated blocks within the data
+// already have been freed.
+void hashtable_free(hashtable_t *table);
+/// ----------
+
 // Opens "[fn].[suffix]" for writing.
 FILE* fopen_derivative(const char *fn, const char *suffix);
 
